d9721931b7b0a3743670cea1443a2f75292506a8
aaronmsimon <aaronsimon@gmail.com>
2022-05-29 23:41:05 -0700

    Add randomness to item spawning

--- Big Boy Vehicles/Assets/Scripts/ItemSpawner.cs
+++ Big Boy Vehicles/Assets/Scripts/ItemSpawner.cs
@@ -5,30 +5,47 @@ using UnityEngine;
 public class ItemSpawner : MonoBehaviour
 {
     [SerializeField] private float timeBetweenSpawns = .5f;
+    [SerializeField] private float spawnRange;
     [SerializeField] public string itemPool;
+    [SerializeField] private bool ignoreIfOverlap;
+    [SerializeField] private float overlapRadius = 1f;
 
     private ObjectPooler objectPooler;
     private float nextItemSpawnTime;
-    [HideInInspector] public bool spawnEnabled;
+    public bool spawnEnabled;
 
     private void Start()
     {
         objectPooler = ObjectPooler.Instance;
-        nextItemSpawnTime = Time.time + timeBetweenSpawns;
+        nextItemSpawnTime = Time.time + timeBetweenSpawns + Random.Range(-spawnRange, spawnRange);
     }
 
     private void Update()
     {
         if (spawnEnabled && Time.time > nextItemSpawnTime)
         {
-            nextItemSpawnTime = Time.time + timeBetweenSpawns;
+            nextItemSpawnTime = Time.time + timeBetweenSpawns + Random.Range(-spawnRange, spawnRange);
             SpawnItem();
         }
     }
 
     private void SpawnItem()
     {
-        objectPooler.SpawnFromPool(itemPool, transform.position, Quaternion.identity);
+        if (ignoreIfOverlap)
+        {
+            objectPooler.SpawnFromPool(itemPool, transform.position, Quaternion.identity);
+        } else
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, overlapRadius);
+            foreach (Collider collider in colliders)
+            {
+                if (collider.name != "Ground")
+                {
+                    return;
+                }
+            }
+            objectPooler.SpawnFromPool(itemPool, transform.position, Quaternion.identity);
+        }
     }
 
 }
