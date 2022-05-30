b6508013eac5bd9073c0c53a0e0296d3ac47e092
aaronmsimon <aaronsimon@gmail.com>
2022-05-29 19:13:52 -0700

    Moved to Static Classes folder
    Not going to use this mesh generator script but wanted to implement noise for size of z-axis

--- Big Boy Vehicles/Assets/Scripts/Static Classes/MeshGenerator.cs
+++ Big Boy Vehicles/Assets/Scripts/Static Classes/MeshGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class MeshGenerator
 {
-    public static MeshData GenerateMesh(float[,] heightMap)
+    public static MeshData GenerateMesh(float[,] heightMap, float heightMultiplier)
     {
         int width = heightMap.GetLength(0);
         int height = heightMap.GetLength(1);
@@ -14,17 +14,26 @@ public static class MeshGenerator
         MeshData meshData = new MeshData(width, height);
         int vertexIndex = 0;
 
+        float[] zAxisNoise = new float[width];
+        for (int i = 0; i < width; i++)
+        {
+            zAxisNoise[i] = heightMap[height - 1, i];
+        }
+
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
-                meshData.vertices[vertexIndex] = new Vector3(topLeftX + x, heightMap[x, y], topLeftZ - y);
-                meshData.uvs[vertexIndex] = new Vector2(x / (float)width, y / (float)height);
-
-                if (x < width - 1 && y < height - 1)
+                if ((float)y / height <= zAxisNoise[x])
                 {
-                    meshData.AddTriangle(vertexIndex, vertexIndex + width + 1, vertexIndex + width);
-                    meshData.AddTriangle(vertexIndex + width + 1, vertexIndex, vertexIndex + 1);
+                    meshData.vertices[vertexIndex] = new Vector3(topLeftX + x, heightMap[x, y] * heightMultiplier, topLeftZ - y);
+                    meshData.uvs[vertexIndex] = new Vector2(x / (float)width, y / (float)height);
+
+                    if (x < width - 1 && y < height - 1)
+                    {
+                        meshData.AddTriangle(vertexIndex, vertexIndex + width + 1, vertexIndex + width);
+                        meshData.AddTriangle(vertexIndex + width + 1, vertexIndex, vertexIndex + 1);
+                    }
                 }
 
                 vertexIndex++;
